b4ebd4f33371e12dcbcea4fd75540db5fa7f4e76
Rohit Dubey <dubey_rohit@outlook.com>
2019-02-23 13:23:16 +0530

    PS01 : Initial Project Commit

---
+++ surveyapp/src/main/java/com/prizy/surveyapp/SurveyappApplication.java
@@ -0,0 +1,15 @@
+package com.prizy.surveyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class SurveyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyappApplication.class, args);
+	}
+
+}
---
+++ surveyapp/src/main/java/com/prizy/surveyapp/controller/ProductSurveyController.java
@@ -0,0 +1,12 @@
+package com.prizy.surveyapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("surveyapp/product")
+public class ProductSurveyController  {
+
+}
